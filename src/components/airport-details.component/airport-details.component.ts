7e95683e77c433ce978e7d1c1fb0dbf3c83e2f5e
Austin Rausch <austin@rauschtech.com>
2025-10-19 22:25:06 -0400

    set up components

---
+++ src/components/airport-details.component/airport-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-airport-details.component',
+  imports: [],
+  templateUrl: './airport-details.component.html',
+  styleUrl: './airport-details.component.css'
+})
+export class AirportDetailsComponent {
+
+}
---
+++ src/components/explore-by-interest.component/explore-by-interest.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-explore-by-interest.component',
+  imports: [],
+  templateUrl: './explore-by-interest.component.html',
+  styleUrl: './explore-by-interest.component.css'
+})
+export class ExploreByInterestComponent {
+
+}
